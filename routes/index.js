537f44b2e972e06407a7481d81ed16a389a0d79a
muhammeddilaver <muhammed.dilaver@gmail.com>
2023-01-08 20:09:07 +0300

    publish

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,39 @@ const Musteriler = require('../models/Musteriler');
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-    res.render("index", { title: "Express" });
+    const {email, sifre} = req.body;
+	
+	Musteriler.findOne({email}, (err, musteri) => {
+		if(err)
+			throw err;
+
+		if(!musteri){
+			res.json({
+				status: false,
+				message: "Authentication user not found."
+			});
+		} else {
+			bcrypt.compare(sifre, musteri.sifre, (err, result) => {
+				if(!result){
+					res.json({
+						status: false,
+						message: 'Authentication is failed, wrong password'
+					});
+				} else {
+					const payload = {
+						email
+					};
+					const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+						expiresIn: 720
+					});
+					res.json({
+						status: true,
+						token
+					})
+				}
+			});
+		}
+	})
 });
 
 router.post('/auth', (req, res) => {
